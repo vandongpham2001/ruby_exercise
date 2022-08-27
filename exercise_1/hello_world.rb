b4ad98c3b4139a0c8ba7f1c15c6b18eccb45aab0
vandongphamtk2001 <vandongphamtk282001@gmail.com>
2022-08-28 00:00:49 +0700

    commit

--- exercise_1/hello_world.rb
+++ exercise_1/hello_world.rb
@@ -1,1 +1,7 @@
-puts "Xin chao Pham Van Dong 1911505310208"
+puts "Xin chào Phạm Văn Đông 1911505310208"
+puts "Nhập số bất kỳ: "
+number = gets
+print "Số vừa nhập là: #{number}"
+puts "Nhập hai số bất kỳ: "
+a, b = gets.split
+puts "Số lớn nhât là: #{a>b ? a : b}" 
