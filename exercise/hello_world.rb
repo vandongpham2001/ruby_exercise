b02685fad0df2c4a6c9124d057fd7cacbf594723
vandongphamtk2001 <vandongphamtk282001@gmail.com>
2022-09-16 10:22:31 +0700

    commit

--- exercise/hello_world.rb
+++ exercise/hello_world.rb
@@ -5,3 +5,15 @@ print "Số vừa nhập là: #{number}"
 puts "Nhập hai số bất kỳ: "
 a, b = gets.split
 puts "Số lớn nhât là: #{a>b ? a : b}" 
+
+s = 0
+s5 = 0
+for i in 1..10 do
+  puts i
+  s += i
+  if (i%5==0) then
+    s5+=i
+  end
+end
+print "Sum is #{s}\n"
+print "Sum5 is #{s5}"
